76f75f98302f4a368e87e0aea809e5309bb438f3
Filip Kolarsky <148553297+FifoKolo@users.noreply.github.com>
2025-12-03 09:56:55 +0000

    fixed_Section

---
+++ OOPProj/src/oopproj/CollectibleInfo.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oopproj;
+
+/**
+ *
+ * @author mncrf
+ */
+public class CollectibleInfo {
+    
+    private String name;
+    
+    private String location;
+    
+    public CollectibleInfo(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+   
+    
+    public String getLocation() {
+        return location;
+    }
+    
+   
+}
